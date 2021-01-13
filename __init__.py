5bb9a2899205d3e060215edae3ec93ab79a58ac6
Olivier Everts <oliviereverts98@gmail.com>
2021-01-13 12:57:10 -0500

    Put everything into functions for easier readability of code

--- __init__.py
+++ __init__.py
@@ -12,6 +12,7 @@ class MyFirstSkill(MycroftSkill):
         self.blocks = ['one block', 'two blocks', 'three blocks']
         self.participant_number = None
         self.tasks = []
+        self.blocks = None
 
     def get_participant_number(self):
         """This function requests the user's participant number. If the user responds with the wrong number
@@ -79,73 +80,7 @@ class MyFirstSkill(MycroftSkill):
             self.speak("Ok let's move on.")
             return self.tasks
 
-    @intent_handler('skill.study.intent')
-    def handle_skill_study(self, message):
-        
-        # Give a welcome message to the user
-        self.speak("Welcome to your study session!" +
-                   " I will be your personal assistant and help you during your studies." +
-                   " Before we begin with the study session, we will first need to do some set up.")
-
-        # Get the participant number from the user using the get_participant_number() function
-        self.participant_number = self.get_participant_number()
-
-        # Get the tasks the user wants to accomplish
-        self.tasks = self.get_tasks()
-
-        # Get the amount of blocks from the user
-        #blocks = self.get_response('blocks.amount.of.blocks')
-        self.speak_dialog("Now that I know the tasks you want to accomplish, let's decide how long you want to study." +
-                          " Each block will last 25 minutes and every break is 5 minutes." +
-                          " How many blocks do you want to study?")
-        blocks = extract_number(self.ask_selection(self.blocks, 'blocks.selection'))
-        while not blocks:
-            self.speak("Sorry, I could not understand you." +
-                       " Do you want to study one block, two blocks or three blocks?" +
-                       " Please respond by choosing one of the options.")
-            blocks = extract_number(self.ask_selection(self.blocks, 'skill.blocks.could.not.understand'))
-
-        # If the user selects one block, Mycroft responds with the singular "block" 
-        if blocks == 1:
-            amount_of_blocks = "1 block"
-        
-        # If the user selects multiple blocks, Mycroft responds with the plural "blocks"
-        if blocks > 1:
-            amount_of_blocks = "{} blocks".format(str(blocks))
-            
-        # Get the current time to calculate the time when the user will be done in the future
-        now = now_local()
-        currenttime = now.strftime("%H:%M")
-        
-        # Calculate the time when the user is done based on the amount of blocks selected
-        # Each block is 25 minutes and each break is 5 minutes
-        
-        minutes = blocks*25 + (blocks-1)*5
-        futuretime = now + timedelta(minutes=minutes)
-        futuretime = futuretime.strftime("%H:%M")
-        
-        # Respond to the user with the amount of blocks they have chosen, what the curren time is
-        # and when their study session will be finished
-        self.speak_dialog('blocks.confirmation', data={'currenttime': currenttime,
-                                                        'amount_of_blocks': amount_of_blocks,
-                                                        'futuretime': futuretime})
-
-        
-        # The time.sleep() function takes an input of seconds
-        # If you want the code to sleep for several minutes you should multiply the values with 60
-        # Currently everythting is in second for debugging purposes
-        for i in range(blocks):
-            time.sleep(25)
-            if i <  blocks-1:
-                self.speak("It's time for a 5 minute break")
-                time.sleep(5)
-                self.speak_dialog("Ok, break time is over. Get back to work.")
-            if i == blocks-1:
-                break
-        self.speak("You have finished your studying session." +
-                   " Let's see what tasks you were able to accomplish during your studying session.")
-
-        # Ask about completion of tasks
+    def completion_of_tasks(self, tasks):
 
         for i in range(len(tasks)):
 
@@ -160,21 +95,91 @@ class MyFirstSkill(MycroftSkill):
             elif completed_task == 'no':
                 self.speak("I am very disappointed, please consider another studying session.")
 
-    @intent_handler('tasks.change.task.intent')
-    def change_task(self, message):
-        selection = self.ask_selection(self.task_names, 'tasks.what.task')
+    def get_blocks(self):
 
-        if selection == 'task one':
-            task1 = self.get_response('tasks.change.task', data={"selection": selection})
-            self.speak_dialog('tasks.task1.confirmation', data={"task1": task1})
+        self.speak("In this study session, we will make use of the pomodoro technique." +
+                   " During the session you will have study blocks of 25 minutes with " +
+                   "a break of 5 minutes between blocks." +
+                   " For this session, do you want to study for:")
+        blocks = extract_number(self.ask_selection(self.blocks, 'blocks.selection'))
+        while not blocks:
+            self.speak("Sorry, I could not understand you." +
+                       " Do you want to study one block, two blocks or three blocks?" +
+                       " Please respond by choosing one of the options.")
+            blocks = extract_number(self.ask_selection(self.blocks, 'skill.blocks.could.not.understand'))
 
-        if selection == 'task two':
-            task2 = self.get_response('tasks.change.task', data={"selection": selection})
-            self.speak_dialog('tasks.task2.confirmation', data={"task2": task2})
+        return blocks
 
-        if selection == 'task three':
-            task3 = self.get_response('tasks.change.task', data={"selection": selection})
-            self.speak_dialog('tasks.task3.confirmation', data={"task3": task3})
+    def blocks_confirmation(self, blocks):
+
+        # If the user selects one block, Mycroft responds with the singular "block"
+        if blocks == 1:
+            amount_of_blocks = "1 block"
+
+        # If the user selects multiple blocks, Mycroft responds with the plural "blocks"
+        if blocks > 1:
+            amount_of_blocks = "{} blocks".format(str(blocks))
+
+        # Get the current time to calculate the time when the user will be done in the future
+        now = now_local()
+        current_time = now.strftime("%H:%M")
+
+        # Calculate the time when the user is done based on the amount of blocks selected
+        # Each block is 25 minutes and each break is 5 minutes
+
+        minutes = blocks * 25 + (blocks - 1) * 5
+        future_time = now + timedelta(minutes=minutes)
+        future_time = future_time.strftime("%H:%M")
+
+        # Respond to the user with the amount of blocks they have chosen, what the curren time is
+        # and when their study session will be finished
+        self.speak_dialog('blocks.confirmation', data={'currenttime': current_time,
+                                                       'amount_of_blocks': amount_of_blocks,
+                                                       'futuretime': future_time})
+
+    def study_time(self, blocks):
+
+        # The time.sleep() function takes an input of seconds
+        # If you want the code to sleep for several minutes you should multiply the values with 60
+        # Currently everythting is in second for debugging purposes
+        for i in range(blocks):
+            time.sleep(25)
+            if i < blocks - 1:
+                self.speak("It's time for a 5 minute break")
+                time.sleep(5)
+                self.speak_dialog("Ok, break time is over. Get back to work.")
+            if i == blocks - 1:
+                break
+        self.speak("You have finished your studying session." +
+                   " Let's see what tasks you were able to accomplish during your studying session.")
+
+    @intent_handler('skill.study.intent')
+    def handle_skill_study(self, message):
+        
+        # Give a welcome message to the user
+        self.speak("Welcome to your study session!" +
+                   " I will be your personal assistant and help you during your studies." +
+                   " Before we begin with the study session, we will first need to do some set up.")
+
+        # Get the participant number from the user using the get_participant_number() function
+        self.participant_number = self.get_participant_number()
+
+        # Get the tasks the user wants to accomplish
+        self.tasks = self.get_tasks()
+
+        self.speak("Now that I know the tasks you want to accomplish, let's decide how long you want to study.")
+
+        # Get the amount of blocks from the user
+        self.blocks = self.get_blocks()
+
+        # Confirm the total study time of the session
+        self.blocks_confirmation(self.blocks)
+
+        # Begin study session
+        self.study_time(self.blocks)
+
+        # Ask about completion of tasks
+        self.completion_of_tasks(self.tasks)
 
 def create_skill():
     return MyFirstSkill()
